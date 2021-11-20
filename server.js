1d5445082782061e10d5260c74ab2c7527ff5081
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2021-11-20 12:24:08 +0300

    Add server.js
    Added nodejs server main executable file.

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios').default;
+
+
+const app = express();
+
+app.use(express.static("./build"));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile('./index.html', { root: './build' });
+});
+
+app.post('/features/', async (req, res) => {
+    let response = await axios.post(process.env.FACE_RECOGNITION, req.body);
+    res.send(response.data);
+});
+
+app.get('/person/:id', async (req, res) => {
+    let response = await axios.get(process.env.PERSON_INFO + req.params.id);
+    res.send(response.data);
+});
+
+app.listen(80);
